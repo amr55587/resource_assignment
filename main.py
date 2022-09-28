e6d061c81611b3ff25dba35090e30ed345e6cc60
Mohamad Osman <eemoh.osman@gmail.com>
2022-09-28 19:31:11 +0300

    the main application is used to run the entire workflow of the code.

--- main.py
+++ main.py
@@ -41,19 +41,19 @@ def home():
     if request.method == "POST":        
         sel_pipeline = request.form["selected_value"]
         return redirect(url_for("optimize", selected_pipeline = sel_pipeline))
-        print("IF")
-        print("\n")
-        print("Selected Pipeline is ",selected_pipeline)
+        #print("IF")
+        #print("\n")
+        #print("Selected Pipeline is ",selected_pipeline)
 
     else:
         pipeline_tasks_list = list(pipeline_tasks.keys())
         for key, value in enumerate(pipeline_tasks_list):
             pipeline_tasks_list[key] = str(value)
-        print("ELSE")
+        #print("ELSE")
 
-        print("\n")
+        #print("\n")
 
-        print("pipeline_tasks_list " , pipeline_tasks_list)
+        #print("pipeline_tasks_list " , pipeline_tasks_list)
         return render_template('home.html', pipeline=pipeline_tasks_list, res_KPIs_stats=res_KPIs_stats)
 
 
@@ -62,21 +62,28 @@ def home():
 def optimize(selected_pipeline):
         
     target_pipeline = list(eval(selected_pipeline))
-    print("Target Pipeline is",target_pipeline)
+    #print("Target Pipeline is",target_pipeline)
     selected_pipeline_tasks = {key: pipeline_tasks[key] for key in pipeline_tasks.keys() if key in target_pipeline}
 
-    print("Selected pipline is ",selected_pipeline_tasks)
     model,ra_dict,pipeline_project_tasks,prod_pipeline_tasks = run_optimization(RESOURCES,selected_pipeline_tasks)
     RESOURCES_future= post_model(model,pipeline_project_tasks,RESOURCES, prod_pipeline_tasks)
-    future_utilization_metrics(RESOURCES_future)
+    dict_assignment_resource_util, overall_future_utilization = future_utilization_metrics(RESOURCES_future)
+    resources = list(dict_assignment_resource_util.keys())
+    ut_factor = list(dict_assignment_resource_util.values())
+    dict_assignment_resource_util_new = {"Resources": resources, "Utilization %": ut_factor}
+    
+    optimization_results = pd.DataFrame.from_dict(dict_assignment_resource_util_new)
+    opt_table = [optimization_results.to_html(classes='cols_rows_df')]
+    opt_title = optimization_results.columns.values
 
     pd_2, hire_res_info = gant_chart_and_hiring_status(ra_dict,prod_pipeline_tasks)
-    
     fig = utils.plot_assignment_gantt_chart(pd_2)
-
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
     
-    return render_template('dashboard.html', graphJSON=graphJSON, hire_res_info = hire_res_info)
+    return render_template('dashboard.html', graphJSON=graphJSON, hire_res_info = hire_res_info,
+                                             tables=opt_table, titles = opt_title, 
+                                             overall_fu = f"{round(overall_future_utilization,2)} %")
 
     
 
